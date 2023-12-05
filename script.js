7ded249fc2c89593e86ffc66a4ef31286c15eb21
Flavia Rosa <144954177+FlavRose@users.noreply.github.com>
2023-12-05 01:54:16 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!'); // aviso de "erro" no console
+        }
+        // "puxando" os elementos do HTML pelos seus IDs para este arquivo js
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        // "chamando" os valores/values de cada elemento para preenchimento
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) { // aviso de "erro" caso usuário digite um CEP inválido
+        mensagemErro.innerHTML = `<p>CEP inválido. Por favor, tente novamente!</p>`
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+// "focusout" serve basicamente para quando o usuário clicar fora do input, ele receber "tal" retorno
